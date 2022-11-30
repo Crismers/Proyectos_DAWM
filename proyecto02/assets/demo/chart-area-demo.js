491c6c442f7f2b4abaddafe5fe76e2f22cc42d99
Crismers <crismers24xd@gmail.com>
2022-11-30 02:03:20 -0500

    chart area

--- proyecto02/assets/demo/chart-area-demo.js
+++ proyecto02/assets/demo/chart-area-demo.js
@@ -1,54 +1,74 @@
 // Set new default font family and font color to mimic Bootstrap's default styling
 Chart.defaults.global.defaultFontFamily = '-apple-system,system-ui,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif';
 Chart.defaults.global.defaultFontColor = '#292b2c';
-
-// Area Chart Example
-var ctx = document.getElementById("myAreaChart");
-var myLineChart = new Chart(ctx, {
-  type: 'line',
-  data: {
-    labels: ["Mar 1", "Mar 2", "Mar 3", "Mar 4", "Mar 5", "Mar 6", "Mar 7", "Mar 8", "Mar 9", "Mar 10", "Mar 11", "Mar 12", "Mar 13"],
-    datasets: [{
-      label: "Sessions",
-      lineTension: 0.3,
-      backgroundColor: "rgba(2,117,216,0.2)",
-      borderColor: "rgba(2,117,216,1)",
-      pointRadius: 5,
-      pointBackgroundColor: "rgba(2,117,216,1)",
-      pointBorderColor: "rgba(255,255,255,0.8)",
-      pointHoverRadius: 5,
-      pointHoverBackgroundColor: "rgba(2,117,216,1)",
-      pointHitRadius: 50,
-      pointBorderWidth: 2,
-      data: [10000, 30162, 26263, 18394, 18287, 28682, 31274, 33259, 25849, 24159, 32651, 31984, 38451],
-    }],
-  },
-  options: {
-    scales: {
-      xAxes: [{
-        time: {
-          unit: 'date'
-        },
-        gridLines: {
-          display: false
-        },
-        ticks: {
-          maxTicksLimit: 7
-        }
-      }],
-      yAxes: [{
-        ticks: {
-          min: 0,
-          max: 40000,
-          maxTicksLimit: 5
-        },
-        gridLines: {
-          color: "rgba(0, 0, 0, .125)",
-        }
-      }],
-    },
-    legend: {
-      display: false
+// Conectamos la API
+const cargarDatosAreaChart = async() => {
+  const respuesta = await fetch('https://inshorts.deta.dev/news?category=science');
+  if(respuesta.status === 200){
+    const data = await respuesta.json();
+    //Buscamos a los autores y su cantidad de publicaciones
+    let date = [];
+    let cant_publi = [];
+    for (element in data["data"]){
+      let info = data["data"][element];
+      let indice = date.indexOf(info["date"]);
+      if(indice === -1){
+        date.push(info["date"]);
+        cant_publi.push(1);
+      }else{
+        cant_publi[indice]+=1;
+      }
     }
+    // Area Chart Example
+    var ctx = document.getElementById("myAreaChart");
+    var myLineChart = new Chart(ctx, {
+      type: 'line',
+      data: {
+        labels: date,
+        datasets: [{
+          label: "Sessions",
+          lineTension: 0.3,
+          backgroundColor: "rgba(2,117,216,0.2)",
+          borderColor: "rgba(2,117,216,1)",
+          pointRadius: 5,
+          pointBackgroundColor: "rgba(2,117,216,1)",
+          pointBorderColor: "rgba(255,255,255,0.8)",
+          pointHoverRadius: 5,
+          pointHoverBackgroundColor: "rgba(2,117,216,1)",
+          pointHitRadius: 50,
+          pointBorderWidth: 2,
+          data: cant_publi,
+        }],
+      },
+      options: {
+        scales: {
+          xAxes: [{
+            time: {
+              unit: 'date'
+            },
+            gridLines: {
+              display: false
+            },
+            ticks: {
+              maxTicksLimit: 7
+            }
+          }],
+          yAxes: [{
+            ticks: {
+              min: 0,
+              max: 15,
+              maxTicksLimit: 5
+            },
+            gridLines: {
+              color: "rgba(0, 0, 0, .125)",
+            }
+          }],
+        },
+        legend: {
+          display: false
+        }
+      }
+    });
   }
-});
+}
+cargarDatosAreaChart();
--- proyecto02/assets/demo/chart-bar-demo.js
+++ proyecto02/assets/demo/chart-bar-demo.js
@@ -1,45 +1,68 @@
 // Set new default font family and font color to mimic Bootstrap's default styling
 Chart.defaults.global.defaultFontFamily = '-apple-system,system-ui,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif';
 Chart.defaults.global.defaultFontColor = '#292b2c';
-// Bar Chart Example
-var ctx = document.getElementById("myBarChart");
-var myLineChart = new Chart(ctx, {
-  type: 'bar',
-  data: {
-    labels: ["January", "February", "March", "April", "May", "June"],
-    datasets: [{
-      label: "Revenue",
-      backgroundColor: "rgba(2,117,216,1)",
-      borderColor: "rgba(2,117,216,1)",
-      data: [4215, 5312, 6251, 7841, 9821, 14984],
-    }],
-  },
-  options: {
-    scales: {
-      xAxes: [{
-        time: {
-          unit: 'month'
-        },
-        gridLines: {
-          display: false
-        },
-        ticks: {
-          maxTicksLimit: 6
-        }
-      }],
-      yAxes: [{
-        ticks: {
-          min: 0,
-          max: 15000,
-          maxTicksLimit: 5
-        },
-        gridLines: {
-          display: true
-        }
-      }],
-    },
-    legend: {
-      display: false
+
+//Cargamos datos de la API
+const cargarDatosBarChart = async() => {
+  const respuesta = await fetch('https://inshorts.deta.dev/news?category=science');
+  if(respuesta.status === 200){
+    const data = await respuesta.json();
+    //Buscamos a los autores y su cantidad de publicaciones
+    let autores = [];
+    let cant_publi = [];
+    for (element in data["data"]){
+      let info = data["data"][element];
+      let indice = autores.indexOf(info["author"]);
+      if(indice === -1){
+        autores.push(info["author"]);
+        cant_publi.push(1);
+      }else{
+        cant_publi[indice]+=1;
+      }
     }
+
+    var ctx = document.getElementById("myBarChart");
+    var myLineChart = new Chart(ctx, {
+      type: 'bar',
+      data: {
+        labels: autores,
+        datasets: [{
+          label: "Publications",
+          backgroundColor: "rgba(2,117,216,1)",
+          borderColor: "rgba(2,117,216,1)",
+          data: cant_publi,
+        }],
+      },
+      options: {
+        scales: {
+          xAxes: [{
+            time: {
+              unit: 'author'
+            },
+            gridLines: {
+              display: false
+            },
+            ticks: {
+              maxTicksLimit: autores.length
+            }
+          }],
+          yAxes: [{
+            ticks: {
+              min: 0,
+              max: 8,
+              maxTicksLimit: 5
+            },
+            gridLines: {
+              display: true
+            }
+          }],
+        },
+        legend: {
+          display: false
+        }
+      }
+    });
   }
-});
+}
+// Bar Chart 
+cargarDatosBarChart();
